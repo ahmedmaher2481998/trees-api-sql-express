ccac12a33e44f47e660d7d5c5a4ecd340a29778b
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-16 15:40:04 +0200

    solved

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ const dotenv = require("dotenv");
 const sqlite3 = require("sqlite3");
 
 dotenv.config();
+
+// instantiate sqlite and database
+const db = new sqlite3.Database(
+	process.env.DATABASE_SOURCE,
+	sqlite3.OPEN_READWRITE
+);
+
+const app = express();
+app.get("/trees", (req, res, next) => {
+	const sql = "select * from trees;";
+	let params = [];
+	db.all(sql, params, (err, rows) => {
+		if (err) {
+			next(err);
+		}
+		res.json(rows);
+	});
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log("Listening for port :" + PORT));
