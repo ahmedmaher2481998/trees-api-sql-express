7df233426472514d32d57a54c7c2a770e5cf397a
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-16 19:27:24 +0200

    solved

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const sqlite3 = require("sqlite3");
+const sqlite3 = require("sqlite3").verbose();
 
 const app = express();
 
