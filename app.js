7d1050185337bf0de83a10ac7dceead46b3aa21e
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-16 16:08:12 +0200

    adding / deleting is working

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const dotenv = require("dotenv");
 const sqlite3 = require("sqlite3");
 
+const app = express();
+
 dotenv.config();
 
 // instantiate sqlite and database
@@ -10,7 +12,8 @@ const db = new sqlite3.Database(
 	sqlite3.OPEN_READWRITE
 );
 
-const app = express();
+app.use(express.json());
+
 app.get("/trees", (req, res, next) => {
 	const sql = "select * from trees;";
 	let params = [];
@@ -20,6 +23,52 @@ app.get("/trees", (req, res, next) => {
 		}
 		res.json(rows);
 	});
+});
+
+app.get("/trees/:id", (req, res, next) => {
+	const sql = "select * from trees where id = ?";
+	let params = [req.params.id];
+	db.get(sql, params, (err, row) => {
+		if (err) {
+			next(err);
+		} else {
+			res.json(row);
+		}
+	});
+});
+
+app.post("/trees", (req, res, next) => {
+	const sql = `INSERT INTO trees (name , tree_number ) VALUES (?,?)`;
+	const sqlLast = "SELECT * FROM trees ORDER BY id DESC LIMIT 1";
+
+	let params = [req.body.name, req.body.number];
+
+	console.log("params", params);
+
+	db.run(sql, params, (err) => {
+		if (err) next(err);
+		else
+			db.get(sqlLast, [], (err, row) => {
+				res.json(row);
+			});
+	});
+});
+
+app.delete("/trees/:id", (req, res, next) => {
+	const sql = `DELETE FROM trees WHERE id = ?`;
+	params = [req.params.id];
+
+	db.run(sql, params, (err) => {
+		if (err) next(err);
+		else {
+			db.all("select * from trees", [], (err, rows) => {
+				res.json(rows);
+			});
+		}
+	});
+});
+app.use((err, req, res, next) => {
+	res.json({ error: "An Error Has happened", msg: err.message });
 });
 
 const PORT = 5000;
